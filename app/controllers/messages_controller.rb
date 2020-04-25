baa8e82d22415a04bc23d3e6beed3cb7d38e9dc5
iaia <iaia72160@gmail.com>
2020-04-26 01:29:47 +0900

    wip

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,14 @@
 class MessagesController < ApplicationController
   def create
-    current_user.messages.create(
+    @message = current_user.messages.create(
       body: message_params[:body],
       room: current_room
     )
+    if @message.new_record?
+      ActionCable.server.broadcast 'room_channel',
+        content: @message.body
+    else
+    end
   end
 
   private
