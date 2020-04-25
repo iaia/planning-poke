e1e6957f7cb1d9a227f0224c7bbe7bc9e3783fb8
iaia <iaia72160@gmail.com>
2020-04-26 00:58:25 +0900

    Add user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_25_073535) do
+ActiveRecord::Schema.define(version: 2020_04_25_155715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2020_04_25_073535) do
   create_table "rooms", force: :cascade do |t|
     t.string "name", null: false
     t.string "password_digest", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
