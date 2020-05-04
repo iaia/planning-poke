ea34b46813d87807561592249e950757f4aca934
iaia <iaia72160@gmail.com>
2020-05-05 00:08:17 +0900

    Add room show

---
+++ app/javascript/packs/rooms/room_show.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="card issue-#{issue.id}">
+    <div class="card-content">
+      <span class="card-title">
+        {{ issue.issue_number }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+    }
+  }
+}
+</script>
---
+++ app/javascript/packs/rooms/show.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './room_show'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const app = new Vue({
+    render: h => h(App)
+  }).$mount()
+  document.body.appendChild(app.$el)
+
+  console.log(app)
+})
+
