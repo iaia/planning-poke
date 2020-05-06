f98a368d320cb22c73e4636f75def1e2e0474006
iaia <iaia72160@gmail.com>
2020-05-06 09:01:30 +0900

    Add todo

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   resources :rooms, only: %i[new index create show]
   resources :messages, only: %i[new create]
   resources :issues, only: %i[index create]
+  # TODO: rooms/#{room_id}/issues/#{issue_id}/estimates にしたい
+  # roomは自明なのでissuesだけで良いかも あまりネストしたくないし
   resources :estimates, only: %i[create]
 
   root to: 'users#new'
