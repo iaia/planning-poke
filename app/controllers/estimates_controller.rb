5cbea4569d584d73b12df50bc70ec96ad6d65b33
iaia <iaia72160@gmail.com>
2020-07-05 23:07:48 +0900

    Send when created estimates

--- app/controllers/estimates_controller.rb
+++ app/controllers/estimates_controller.rb
@@ -5,10 +5,11 @@ class EstimatesController < ApplicationController
     @estimate = Estimate.create_or_find_by_issue_id(
       current_user, params[:issue_id], estimate_params[:point]
     )
-    return if current_issue.estimates.pluck(:user_id).size != current_room.user_count
-
-    current_issue.update!(done: true) # FIXME: false想定
     RoomChannel.send_estimates(current_room.id, current_issue)
+    if current_issue.estimates.pluck(:user_id).size != current_room.user_count
+    else
+      current_issue.update!(done: true) # FIXME: false想定
+    end
   end
 
   private
