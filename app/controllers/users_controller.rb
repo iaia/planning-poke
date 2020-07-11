b0c2111f3f94e16e920de787d677130d3a0938d6
iaia <iaia72160@gmail.com>
2020-07-12 03:29:25 +0900

    Redirect after logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,12 @@ class UsersController < ApplicationController
     @user = User.create!(user_params)
     session[:user_id] = @user.id
 
+    if session[:redirect_url_after_logged_in]
+      url = session[:redirect_url_after_logged_in]
+      session[:redirect_url_after_logged_in] = nil
+      redirect_to url and return
+    end
+
     respond_to do |format|
       format.html { redirect_to new_room_path }
       format.json { @user }
