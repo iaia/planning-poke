f504d2761bf57f919b16b05c9d1e4c673750f1bd
iaia <iaia72160@gmail.com>
2020-09-01 20:50:10 +0900

    Configure bullet

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,4 +61,9 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # bullet
+  config.after_initialize do
+    Bullet.enable = true
+  end
 end
